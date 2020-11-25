e83ac1d73520e4d82e298a26aa56fe91d1e14f1a
Jibanyan-FCU <d0745587@mail.fcu.edu.tw>
2020-11-25 14:28:44 +0800

    分軌、判斷並增加和弦（未改變樂器，彈性度還不夠高）

---
+++ addTrack.py
@@ -0,0 +1,93 @@
+import music21
+from sys import argv
+import os
+import glob
+import re
+import musicTheory
+
+class NotMidFile(Exception):
+    pass
+
+usage =\
+'''
+Usage:
+    python addTrack.py <midi_file.mid>
+'''
+
+# beats pre measure
+b = 4
+quarterLengt = 2
+
+def addTrack(midi_file, addRhythm:bool=False):
+    midi_file = music21.converter.parse(midi_file)
+    
+    # 將單旋律的所有音符分離出來
+    melody = [element for element in midi_file.recurse() if isinstance(element, music21.note.Note)]
+    for note in melody:
+        note = music21.note.Note(note.nameWithOctave)
+    for note in melody:    
+        note.quarterLength = 0.5
+    print(melody[1].quarterLength)
+    melody_part = music21.stream.Part(melody)
+    # 增加節奏
+    '''
+    if addRhythm:
+        rhythm = musicTheory.combineRhythm(len(melody))
+        for note, beat in melody, rhythm:
+            note.offset = beat
+    '''
+
+    # 為每 4 拍增加和弦
+    #chords = [music21.chord.Chord(melody[i*b:i*b+b]) for i in range(len(melody) // b)]
+    chords = []
+    for i in range(len(melody) // b):
+        chords.append(musicTheory.makeChord(melody[i*b:i*b+b], quarterLengt=2))
+        '''
+        #getNotes = melody[i*b:i*b+b]
+        getNotes = []
+        for j in range(i*b, i*b+b):
+            getNotes.append(music21.note.Note(melody[j].nameWithOctave))
+
+        new_chord = music21.chord.Chord(getNotes)
+        chords.append(new_chord)
+        '''
+
+    offset = 0
+    for chord in chords:
+        for note in chord:
+            note.octave = 3
+            pass
+        chord.offset = offset
+        offset += quarterLengt
+        # print(chord)
+    
+    chord_part = music21.stream.Part(chords)
+    
+    #part1 = music21.stream.Part(melody)
+    #part2 = music21.stream.Part(chords)
+
+    sc = music21.stream.Score()
+    sc.insert(0, melody_part)
+    sc.insert(0, chord_part)
+
+    midi_stream = music21.stream.Stream(sc)
+    midi_stream.write('midi', fp='./test_output.mid')
+    print('done')
+    # make 
+
+
+if __name__ == '__main__':
+    try:
+        midi_files = glob.glob(argv[1])
+        if len(midi_files) == 0:
+            raise FileNotFoundError('Target file is not existed.')
+
+        addTrack(midi_files[0])
+
+    except IndexError as e:
+        print(usage)
+        
+'''
+    a = music21.converter.parse('./test_output.mid')
+    print(a.track)
+'''
--- musicTheory.py
+++ musicTheory.py
@@ -1,10 +1,18 @@
+import music21
 import json
 import random
 import math
+import numpy as np
+
 
 rhythm = []
+chord = {}
+
 with open('./musicTheory/rythem.json','r') as _:
     rythem = json.loads(_.read())
+
+with open('./musicTheory/chord.json','r') as _:
+    chord = json.loads(_.read())
 
 def combineRhythm(beats:int=1):
     combList = []
@@ -16,6 +24,45 @@ def combineRhythm(beats:int=1):
         combList = combList + randList2
         beats -= rythem.index(randList1)
     return combList
-# print(combineRythem(4))
 
-#print(type(rythem[1][0][0]))
+def makeChord(notes, root=music21.note.Note('C'), quarterLengt=4):
+    countNote = np.zeros(12, dtype=int)
+    major = [(i in chord['Maj']) for i in range(12)]
+    minor = [(i in chord['Min']) for i in range(12)]
+    currChord = None
+    max = 0
+    
+    for note in notes:
+        countNote[int(note.pitch.ps)%12] += 1
+    #print(countNote)
+    # 利用 mask 做比對
+    
+    for _ in range(12):
+        # 大和弦
+        sum = 0
+        for i in range(12):
+            if major[i]:
+                sum += countNote[i]
+        if sum > max:
+            max = sum
+            currChord = major[:]
+        major = major[-1:] + major[:-1]
+        
+        # 小和弦
+        sum = 0
+        for i in range(12):
+            if minor[i]:
+                sum += countNote[i]
+        if sum > max:
+            max = sum
+            currChord = minor[:]
+        minor = minor[-1:] + minor[:-1]
+    
+    newChord = music21.chord.Chord([music21.note.Note(i) for i in range(12) if currChord[i] == True], quarterLength=quarterLengt)
+    return newChord
+
+'''
+note = music21.note.Note('C')
+print(note.pitch)
+print(int(note.pitch.ps))
+'''
